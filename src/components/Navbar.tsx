4c22f2fe1ae500b913db8ffe8ac968613b3e7349
Prateek Soni <prateek_soni@outlook.com>
2022-07-03 02:00:55 +0530

    init chess
    Signed-off-by: Prateek Soni <prateek_soni@outlook.com>

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className="h-16 flex items-center w-3/5 mx-auto">
+      <div className="text-2xl font-extrabold tracking-tight">Chess</div>
+    </nav>
+  );
+};
+
+export default Navbar;
