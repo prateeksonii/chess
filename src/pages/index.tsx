2359a163ac115587eec74fd442f595f3ca81a0e5
Prateek Soni <prateek_soni@outlook.com>
2022-07-03 02:01:49 +0530

    remove trpc call
    Signed-off-by: Prateek Soni <prateek_soni@outlook.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
-import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
-
   return (
     <>
       <Head>
